ffa79ed60f54d0c8b3cbdd02ce0505d09c354c19
miftah1299 <jannat27120@gmail.com>
2024-10-03 17:21:19 +0600

    added logicaalOperator.js

---
+++ logicalOperator.js
@@ -0,0 +1,31 @@
+
+
+// logical operator
+// 1. AND (&&)
+// 2. OR (||)
+// 3. NOT (!)
+
+
+// AND (&&)
+// if left side is true, then return right side
+const money = 100;
+console.log(money && 'I have money'); 
+
+/*
+
+
+*/
+// OR (||)
+// if left side is false, then return right side
+const name = '';
+console.log(name || 'Anonymous');
+
+/*
+
+
+*/
+// NOT (!)
+// if true, then return false
+const isTrue = true;
+console.log(!isTrue);
+
